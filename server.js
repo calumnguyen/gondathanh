4c64a88ef4e20d5e8a3bbe76db951aedac54db15
Calum Nguyen <calumengee@calums-air.lan>
2020-11-29 23:04:22 -0800

    fix port access

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 3000);
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+app.use(express.static('public'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+//Confirmation for 'node index.js' or 'nodemon'
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Port is running...');
+});
